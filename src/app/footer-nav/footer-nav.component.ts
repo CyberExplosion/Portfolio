d1fb5e86109f1461e4ea85e1e9dc028f481bc95f
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-07-14 01:26:09 -0700

    the domsanitizer is infiite load
    Need to look closer to what's going on

--- src/app/footer-nav/footer-nav.component.ts
+++ src/app/footer-nav/footer-nav.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { ContactInfos, IContactInfo } from '../my-contacts';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { FooterInfo, IFooterInfo } from '../my-footer';
 @Component({
   selector: 'app-footer-nav',
   templateUrl: './footer-nav.component.html',
   styleUrls: ['./footer-nav.component.css']
 })
 export class FooterNavComponent {
-  contactList: IContactInfo[] = ContactInfos;
+  constructor(private sanitizer: DomSanitizer) { }
+  
+  sanitizeSVG (svgHtml: string): SafeHtml{
+    return this.sanitizer.bypassSecurityTrustHtml(svgHtml);
+  }
+  footerInfos: IFooterInfo[] = FooterInfo;
 }
