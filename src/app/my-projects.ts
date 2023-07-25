f34da4c3ca400ebf95cfa7410a3f89777a5c24a3
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-07-25 02:10:56 -0700

    Get rid of placeholder
    Now this turn official. I only update the project section now

--- src/app/my-projects.ts
+++ src/app/my-projects.ts
@@ -25,38 +25,4 @@ export const ProjectInfos = [
             link: "https://github.com/CyberExplosion/Fall-King"
         }
     },
-    {
-        imgPath: 'assets\\navIcons\\Image22.jpg',
-        title: 'CryptoCrowd Sentiment Analysis',
-        techList: ['Amazon', 'TypeScript', 'React'],
-        description: "Cryptocurrency sentiment analysis on Twitter posts. Users can see the overall sentiment (positive, negative or neutral) of different Cryptocurrencies based on tweets. Users can also view keywords and an average sentiment score. React frontend and Node.js backend. Stateless application that uses Elasticache and S3 on AWS for persistence. Application was setup with an autoscaling group to scale in and out as needed.",
-        mainButton: {
-            logo: "lightBulb",
-            text: "Demo",
-            link: "#"
-        },
-        sideButton: {
-            logo: "github",
-            text: "Code",
-            link: "#"
-        }
-    },
-    {
-        imgPath: 'assets\\navIcons\\Image22.jpg',
-        title: 'CryptoCrowd Sentiment Analysis',
-        techList: ['Amazon', 'TypeScript', 'React'],
-        description: "Cryptocurrency sentiment analysis on Twitter posts. Users can see the overall sentiment (positive, negative or neutral) of different Cryptocurrencies based on tweets. Users can also view keywords and an average sentiment score. React frontend and Node.js backend. Stateless application that uses Elasticache and S3 on AWS for persistence. Application was setup with an autoscaling group to scale in and out as needed.",
-        mainButton: {
-            logo: "lightBulb",
-            text: "Demo",
-            link: "#"
-        },
-        sideButton: {
-            logo: "github",
-            text: "Code",
-            link: "#"
-        }
-    },
-
-
 ]
