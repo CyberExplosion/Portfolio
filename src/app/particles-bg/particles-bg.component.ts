55dfde10e46d06cf305a52eff5576cb937f4de24
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-08-07 20:43:29 -0700

    Done for now, need to do more investigate in the triangle animation

--- src/app/particles-bg/particles-bg.component.ts
+++ src/app/particles-bg/particles-bg.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
+import { NgParticlesComponent } from 'ng-particles';
 import {
-  MoveDirection, ClickMode,
-  HoverMode,
-  OutMode,
+  Engine,
 } from 'tsparticles-engine';
-
-
+import type { ISourceOptions } from "tsparticles-engine";
+import { loadLinksPreset } from 'tsparticles-preset-links';
 @Component({
   selector: 'app-particles-bg',
   templateUrl: './particles-bg.component.html',
   styleUrls: ['./particles-bg.component.css']
 })
-export class ParticlesBgComponent {
 
+  
+// Triangle options: https://github.com/tsparticles/presets/blob/main/presets/triangles/src/options.ts
+export class ParticlesBgComponent {
+  particlesOptions: ISourceOptions = {
+    backgroundMode: ,
+    particles: {
+    },
+    shape: {
+      type: "square", // starting from v2, this require the square shape script
+    },
+    interactivity: {
+      events: {
+        onClick: {
+          detectsOn: "window",
+          enable: true,
+          mode: "grab"
+        }
+      }
+    },
+    fpsLimit: 120,
+    preset: "links",
+  };
+
+  particlesInit = async (engine: Engine): Promise<void> => {
+    console.log(engine);
+
+    await loadLinksPreset(engine);
+  }
 }
